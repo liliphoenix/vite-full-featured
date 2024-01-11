819dd853e03807f393a984828a31384aa73d17b7
int.liuwenbo <1090851297@qq.com>
2024-01-11 17:45:43 +0800

    :sparkles: feat(css): scss规范化配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        'plugin:vue/recommended',
-        "standard-with-typescript",
-        "plugin:vue/vue3-essential",
-        "plugin:@typescript-eslint/recommended",
-        'plugin:prettier/recommended' 
-    ],
-    "overrides": [
-        
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "vue-eslint-parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module" ,
-        "parser": "@typescript-eslint/parser",
-        "extraFileExtensions":['.vue']
-    },
-    "plugins": [
-        "vue","@typescript-eslint"
-    ],
-    "rules": {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'standard-with-typescript',
+    'plugin:vue/vue3-essential',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
     }
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    extraFileExtensions: ['.vue']
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {}
 }
