42315795a0dde4ddca0f229cc7b9a3e6616019e0
int.liuwenbo <1090851297@qq.com>
2024-01-12 14:23:44 +0800

    :sparkles: feat: vite配置文件更新

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import path from 'path'
 export default defineConfig({
   plugins: [
     // ...
@@ -20,5 +21,35 @@ export default defineConfig({
         additionalData: '@import "./src/assets/main.scss";'
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@types': path.resolve(__dirname, 'src/types')
+    },
+    extensions: ['.js', '.cjs', '.json', '.ts', '.vue']
+  },
+  optimizeDeps: {
+    force: true // 强制进行依赖预构建
+  },
+  server: {
+    host: true, // 在局域网内进行热更新,
+    proxy: {
+      '/api': {
+        target: process.env.VUE_CLIENT_HOST,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  // 配置静态资源基础路径
+  base: process.env.NODE_ENV === 'development' ? '' : process.env.ASSETS_PATH,
+  build: {
+    outDir: '/dist',
+    assetsDir: '/static',
+    assetsInlineLimit: 4096
   }
 })
