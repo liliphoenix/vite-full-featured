4ad68dee4882965923096600d80cb183cd2def42
int.liuwenbo <1090851297@qq.com>
2024-03-11 16:14:57 +0800

    :bug: fix: vite-press config change

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'vite-full-featured',
   description: 'A VitePress Site',
+  base:'/vite-full-featured/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
