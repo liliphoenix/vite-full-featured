9d27d3527794baa0b8af147d96486398e1b4110e
int.liuwenbo <1090851297@qq.com>
2024-03-11 15:54:01 +0800

    :construction_worker: ci: config change

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "vite-full-featured",
-  description: "A VitePress Site",
+  title: 'vite-full-featured',
+  description: 'A VitePress Site',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
